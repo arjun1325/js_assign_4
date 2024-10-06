63454e90d4c7f220579a4ae5a47a727950e995fa
Arjun M <arjunajuoff@gmail.com>
2024-10-06 23:59:19 +0530

    changes added

--- script.js
+++ script.js
@@ -2,34 +2,53 @@ $("#form-account").validate({
     rules: {
         name: {
             required: true,
-            minlength: 2,
+            minlength: 3,
+            maxlength: 50
         },
         gender: {
             required: true,
+            minlength: 4,
+            maxlength: 15
         },
         age: {
-            required: true
+            required: true,
+            maxlenght:3
         },
         phone: {
-            required: true
+            required: true,
+            minlenght: 10,
+            maxlenght: 14
         },
         email: {
             required: true
         },
         address: {
-            required: true
+            required: false,
+            maxlenght:50
         }
     },
     messages: {
         name: {
             required: "Name  is required",
-            minlength: "Mininum 2 charecters"
+            minlength: "Mininum 2 charecters",
+            maxlength: "Do not exceed 50 characters"
         },
         gender: {
-            required: "Please Enter Your Gender"
+            required: "Please Enter Your Gender",
+            minlength: "minmum 4 characters",
+            maxlength: "maximum 15 characters"
         },
         age: {
-            required: "Enter your age"
+            required: "should be above 18"
+        
+        },
+        address: {
+            maxlenght:"Do not exceed 50 charatcters"
+        },
+        phone: {
+            required: "Please enter your Phone Number ",
+            minlenght: "minimum 10 digits",
+            maxlenght: "maximum 14 digits"
         },
 
     }
