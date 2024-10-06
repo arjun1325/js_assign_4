e2777afeeb1480f2bf83923d005b7af9e353c163
Arjun M <arjunajuoff@gmail.com>
2024-10-06 23:40:19 +0530

    validation completed

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-$("#form-account").validate()
+$("#form-account").validate({
+    rules: {
+        name: {
+            required: true,
+            minlength: 2,
+        },
+        gender: {
+            required: true,
+        },
+        age: {
+            required: true
+        },
+        phone: {
+            required: true
+        },
+        email: {
+            required: true
+        },
+        address: {
+            required: true
+        }
+    },
+    messages: {
+        name: {
+            required: "Name  is required",
+            minlength: "Mininum 2 charecters"
+        },
+        gender: {
+            required: "Please Enter Your Gender"
+        },
+        age: {
+            required: "Enter your age"
+        },
+
+    }
+}
+
+)
